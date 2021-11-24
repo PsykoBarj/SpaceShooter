0711b6927f0d38a4eef515aef2b83cfc52161336
Yoann LANDOUSIES <yoann.landousies@lycee-basch.fr>
2021-11-24 17:58:28 +0100

    Empecher le vaisseau de sortir a droite et en bas

--- SpaceShooter.py
+++ SpaceShooter.py
@@ -16,14 +16,14 @@ class Spaceship(object):
         self.y = 200
     def key(self):
         keys = pygame.key.get_pressed()
-        vel = 5
+        vel = 10
         if keys[pygame.K_LEFT] and self.x>0:
             self.x -= vel
-        if keys[pygame.K_RIGHT] and self.x<500:
+        if keys[pygame.K_RIGHT] and self.x<500-80:
             self.x += vel 
         if keys[pygame.K_UP] and self.y>0:
             self.y -= vel  
-        if keys[pygame.K_DOWN] and self.y<500:
+        if keys[pygame.K_DOWN] and self.y<500-80:
             self.y += vel
     def draw(self, surface):
         surface.blit(self.image, (self.x,self.y))
