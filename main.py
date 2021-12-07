117b318bc82992336d9961ddc0774495cd274f7d
Yoann LANDOUSIES <yoann.landousies@lycee-basch.fr>
2021-12-07 19:02:17 +0100

    Changement de position depart du joueur et rajout de code dans main pour le joueur

--- main.py
+++ main.py
@@ -6,17 +6,34 @@ class Game:
         self.screen = screen
         self.running = True
         self.clock = pygame.time.Clock()
+        self.player = Player(500, 500)
 
     def handling_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
 
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.player.velocity[0] = -1
+        elif key[pygame.K_RIGHT]:
+            self.player.velocity[0] = 1
+        else:
+            self.player.velocity[0] = 0
+
+        if key[pygame.K_UP]:
+            self.player.velocity[1] = -1
+        elif key[pygame.K_DOWN]:
+            self.player.velocity[1] = 1
+        else:
+            self.player.velocity[1] = 0
+
     def update(self):
-        pass
+        self.player.move()
 
     def display(self):
         self.screen.fill("black")
+        self.player.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
